7c1356bf574e1f933c174fcbc43922071e32d2b2
tobias.hartwich <tobias.hartwich@hotmail.de>
2020-11-12 12:18:58 +0100

    erste Version mit allen Funktionen ausser Hint

---
+++ Game.cs
@@ -0,0 +1,99 @@
+﻿namespace TicTacToe
+{
+    partial class Program                               // *** TicTacToe von TobiH ***
+    {
+        class Game
+        {
+            public FieldState[,] board = new FieldState[3, 3];
+            public bool currentPlayerID = false;
+            public string[] playerNames = new string[2];
+            public int turnNumber = 1;
+
+            public FieldState[,] getBoard()
+            {
+                return board;
+            }
+            public bool getPlayerID()
+            {
+                return currentPlayerID;
+            }
+            public TurnResult turn(Point point)
+            {
+                TurnResult result = TurnResult.Valid;
+                FieldState playerFieldState = FieldState.X;
+
+                if (currentPlayerID == true) playerFieldState = FieldState.O;
+
+                if (board[point.y, point.x] != FieldState.E)
+                {
+                    return result;
+                }
+
+                board[point.y, point.x] = playerFieldState;
+                if (checkWin(playerFieldState))
+                {
+                    result = TurnResult.Win;
+                }
+
+                turnNumber++;
+
+                if (turnNumber == 10) return TurnResult.Tie;
+
+                currentPlayerID = !currentPlayerID;
+                return result;
+            }
+
+            private bool checkWin(FieldState playerFieldState)
+            {
+                int SteineZaehlerX = 0;                     // für den horizontalen Test  
+                int SteineZaehlerY = 0;                     // für den vertikalen Test
+                int SteineZaehlerDiagonal1 = 0;             // für den Test von oben links nach unten rechts
+                int SteineZaehlerDiagonal2 = 0;             // für den Test von oben rechts nach unten links
+
+                for (int y = 0; y <= 2; y++)                // Haupt-Testschleife, wir testen alles in einem Rutsch
+                {
+                    SteineZaehlerX = 0;
+                    SteineZaehlerY = 0;
+                    SteineZaehlerDiagonal1 = 0;
+                    SteineZaehlerDiagonal2 = 0;
+
+                    for (int x = 0; x <= 2; x++)            // wir erhöhen x in jedem Durchgang
+                    {
+                        if (board[y, x] == playerFieldState)      // horizontaler Test, fängt an bei [0,0] [0,1] [0,2] ...
+                        {
+                            SteineZaehlerX++;               // Anzahl der gleichen Steine in dieser Reihe um 1 erhöhen
+                        }
+                        if (board[x, y] == playerFieldState)      // vertikaler Test (wir tauschen einfach x und y), Test fängt an bei [0,0] [1,0] [2,0] ...
+                        {
+                            SteineZaehlerY++;               // Anzahl der gleichen Steine in dieser Spalte um 1 erhöhen
+                        }
+                        if (board[x, x] == playerFieldState)      // Test diagonal von oben links, x wird immer um 1 erhöht daher setzen wir einfach [x,x] ein, das ergibt den Test für [0,0] [1,1] [2,2]
+                        {
+                            SteineZaehlerDiagonal1++;       // Anzahl der gleichen Steine in dieser Diagonalen um 1 erhöhen
+                        }
+                        if (board[2 - x, x] == playerFieldState)  // Test diagonal von oben rechts, x wird immer um 1 erhöht, mit [2 - x, x] erhalten wir also [2,0] [1,1] [0,2]
+                        {
+                            SteineZaehlerDiagonal2++;       // Anzahl der gleichen Steine in dieser Diagonalen um 1 erhöhen
+                        }
+                        if (SteineZaehlerX == 3 || SteineZaehlerY == 3 || SteineZaehlerDiagonal1 == 3 || SteineZaehlerDiagonal2 == 3) // Sobald irgendwo 3 gleiche Steine gezählt wurden, dann ...
+                        {
+                            return true;         // raus aus der Funktion, Gewinner steht fest, kein weiteres Prüfen notwendig
+                        }
+                    }
+                }
+                return false;
+            }
+            public void DrawHint (bool player)
+            {
+            }
+            public byte GetHint(bool player)
+            {
+                return 1;
+            }
+            public void ResetBoard()
+            {
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace TicTacToe
 {
-    class Program                               // *** TicTacToe von TobiH ***
+    partial class Program                               // *** TicTacToe von TobiH ***
     {
         enum FieldState
         {
@@ -12,62 +12,45 @@ namespace TicTacToe
         {
             Valid, Invalid, Tie, Win
         }
-        class Point
+        struct Point
         {
-            byte x, y;
-        }
-
-        class Game
-        {
-            public FieldState[,] board = new FieldState[3, 3];
-            public bool currentPlayerID = false;
-            public string[] playerNames = new string[2];
-
-            public FieldState[,] getBoard()
-            {
-                return board;
-            }
-            public bool getPlayerID()
-            {
-                return currentPlayerID;
-            }
-            public TurnResult turn(Point point)
-            {
-                TurnResult result = TurnResult.Tie;
-
-                return result;
-            }
-            public void DrawHint (bool player)
-            {
-            }
-            public byte GetHint(bool player)
-            {
-                return 1;
-            }
-            public void ResetBoard()
-            {
-
-            }
+            public byte x, y;
         }
         static void Main(string[] args)
         {
             Game game = new Game();
-            Draw(game.board);
-            Point point = new Point();
-            int turnNumber = 1;
+            
+            TurnResult result = TurnResult.Invalid;
+            Point point;
 
-            Console.Clear();            // Bildschirm löschen
-            Console.WriteLine("TicTacTow von TobiH");
-            Console.WriteLine("Runde {0}, Spieler {1} ist an der Reihe. Mach dein Spielzug.\n", turnNumber, game.currentPlayerID);
-
-            ConsoleKeyInfo UserInput = new ConsoleKeyInfo();
             do
             {
-                turnNumber++;
-            } while (game.turn(point) != TurnResult.Win && game.turn(point) != TurnResult.Tie && UserInput.Key != ConsoleKey.X);
+                Draw(game.board);
+                do
+                {
+                    Console.WriteLine("Bitte X-Position eingeben: ");
+                    point.x = (byte)EingabePosition();
+                } while (point.x == 3);
+                do
+                {
+                    Console.WriteLine("Bitte Y-Position eingeben: ");
+                    point.y = (byte)EingabePosition();
+                } while (point.y == 3);
+
+                result = game.turn(point);
+
+            } while (result != TurnResult.Win && result != TurnResult.Tie);
+
+            if (result == TurnResult.Win) Console.WriteLine("gewonnen!");
+            else Console.WriteLine("unentschieden!");
+
 
             void Draw(FieldState[,] board)
             {
+                Console.Clear();
+                Console.WriteLine("TicTacToe von TobiH");
+                Console.WriteLine("Runde {0}, Spieler {1} ist an der Reihe. Mach dein Spielzug.\n", game.turnNumber, game.currentPlayerID);
+
                 Console.WriteLine("\n\n\t    0   1   2  ");
                 Console.WriteLine("\t  -------------");
                 Console.WriteLine("\t0 | {0} | {1} | {2} |", board[0, 0], board[0, 1], board[0, 2]);
@@ -76,6 +59,27 @@ namespace TicTacToe
                 Console.WriteLine("\t  -------------");
                 Console.WriteLine("\t2 | {0} | {1} | {2} |", board[2, 0], board[2, 1], board[2, 2]);
                 Console.WriteLine("\t  -------------\n\n");
+            }
+
+            int EingabePosition()
+            {
+                int Eingabe = 0;
+                try
+                {
+                    Eingabe = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Falsche Eingabe, bitte erneut versuchen.");
+                    return 3;
+                }
+                if (Eingabe >= 0 && Eingabe <= 2)
+                    return Eingabe;
+                else
+                {
+                    Console.WriteLine("Nur Zahlen von 0 - 2 sind erlaubt, bitte erneut versuchen.");
+                    return 3;
+                }
             }
         }
     }
