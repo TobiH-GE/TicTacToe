0ed2b450051c806b077ed2388e7de5fa39217af6
tobias.hartwich <tobias.hartwich@hotmail.de>
2020-11-12 10:47:10 +0100

    erster Upload mit Übernahme von Code aus meinem alten Projekt

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TicTacToe
     {
         enum FieldState
         {
-            Empty, X, O, Hint
+            E, X, O, H
         }
         enum TurnResult
         {
@@ -19,8 +19,8 @@ namespace TicTacToe
 
         class Game
         {
-            FieldState[,] board = new FieldState[3, 3];
-            bool currentPlayerID = false;
+            public FieldState[,] board = new FieldState[3, 3];
+            public bool currentPlayerID = false;
             public string[] playerNames = new string[2];
 
             public FieldState[,] getBoard()
@@ -51,9 +51,31 @@ namespace TicTacToe
         }
         static void Main(string[] args)
         {
-            void Draw(FieldState board)
-            {
+            Game game = new Game();
+            Draw(game.board);
+            Point point = new Point();
+            int turnNumber = 1;
 
+            Console.Clear();            // Bildschirm löschen
+            Console.WriteLine("TicTacTow von TobiH");
+            Console.WriteLine("Runde {0}, Spieler {1} ist an der Reihe. Mach dein Spielzug.\n", turnNumber, game.currentPlayerID);
+
+            ConsoleKeyInfo UserInput = new ConsoleKeyInfo();
+            do
+            {
+                turnNumber++;
+            } while (game.turn(point) != TurnResult.Win && game.turn(point) != TurnResult.Tie && UserInput.Key != ConsoleKey.X);
+
+            void Draw(FieldState[,] board)
+            {
+                Console.WriteLine("\n\n\t    0   1   2  ");
+                Console.WriteLine("\t  -------------");
+                Console.WriteLine("\t0 | {0} | {1} | {2} |", board[0, 0], board[0, 1], board[0, 2]);
+                Console.WriteLine("\t  -------------");
+                Console.WriteLine("\t1 | {0} | {1} | {2} |", board[1, 0], board[1, 1], board[1, 2]);
+                Console.WriteLine("\t  -------------");
+                Console.WriteLine("\t2 | {0} | {1} | {2} |", board[2, 0], board[2, 1], board[2, 2]);
+                Console.WriteLine("\t  -------------\n\n");
             }
         }
     }
