eea90d8a7c91e18b1b5583e3c17ccd6c15304714
tobias.hartwich <tobias.hartwich@hotmail.de>
2020-11-12 21:14:49 +0100

    more comments

--- Game.cs
+++ Game.cs
@@ -38,7 +38,7 @@
                 return TurnResult.Valid;
             }
 
-            private bool checkWin(FieldState playerFieldState, int checkValue = 3)
+            private bool checkWin(FieldState playerFieldState, int checkValue = 3) // zum Test ob mit checkValue Steinen gewonnen
             {
                 int counterX = 0;                     // für den horizontalen Test  
                 int counterY = 0;                     // für den vertikalen Test
@@ -70,7 +70,7 @@
                         {
                             counterDiag2++;       // Anzahl der gleichen Steine in dieser Diagonalen um 1 erhöhen
                         }
-                        if (counterX == checkValue || counterY == checkValue || counterDiag1 == checkValue || counterDiag2 == checkValue) // Sobald irgendwo 3 gleiche Steine gezählt wurden, dann ...
+                        if (counterX == checkValue || counterY == checkValue || counterDiag1 == checkValue || counterDiag2 == checkValue) // Sobald irgendwo checkValue gleiche Steine gezählt wurden, dann ...
                         {
                             return true;         // raus aus der Funktion, Gewinner steht fest, kein weiteres Prüfen notwendig
                         }
@@ -90,7 +90,7 @@
                 if (hint.x != 9)
                     return ($"Hint: " + hint.x + ", " + hint.y);
 
-                hint = GetHint(2, (currentPlayerID ? FieldState.X : FieldState.O)); // prüfe ob Spieler einen zweiten Stein in eine Reihe legen kann
+                hint = GetHint(2, (currentPlayerID ? FieldState.X : FieldState.O)); // prüfe ob Spieler einen zweiten Stein in eine Reihe legen kann und dann mit einem dritten die Reihe gewinnt
                 if (hint.x != 9)
                     return ($"Hint: " + hint.x + ", " + hint.y);
 
@@ -105,31 +105,31 @@
                 {
                     for (byte x = 0; x < 3; x++)
                     {
-                        if (board[y, x] == FieldState.Empty)
+                        if (board[y, x] == FieldState.Empty) // prüfen des Spielfelds auf einen freien Platz
                         {
-                            board[y, x] = fState;
+                            board[y, x] = fState;   // ein Teststein setzen
 
-                            if (checkWin(fState, checkValue))
+                            if (checkWin(fState, checkValue))   // testen ob mit Anzahl checkValue gewonnen wird
                             {
-                                if (checkValue == 3)
+                                if (checkValue == 3) // testen ob 3 Steine in Reihe liegen
                                 {
-                                    returnHint.x = x;
+                                    returnHint.x = x; // Werte x y merken
                                     returnHint.y = y;
-                                    board[y, x] = FieldState.Empty;
-                                    return returnHint;
+                                    board[y, x] = FieldState.Empty; // Teststein wieder vom Feld löschen
+                                    return returnHint; // Werte zurückgeben
                                 }
-                                if (checkValue == 2)
+                                if (checkValue == 2) // testen ob 2 Steine in Reihe liegen
                                 {
-                                    returnHint = GetHint(3, fState);
-                                    board[y, x] = FieldState.Empty;
-                                    if (returnHint.x != 9) return returnHint;
+                                    returnHint = GetHint(3, fState); // testen ob mit einem 3 Stein ein Gewinn möglich
+                                    board[y, x] = FieldState.Empty; // 2. Teststein wieder vom Feld löschen
+                                    if (returnHint.x != 9) return returnHint; // Gewinn mit 3. möglich
                                 }
                             }
-                            board[y, x] = FieldState.Empty;
+                            board[y, x] = FieldState.Empty; // kein Gewinn möglich, Teststein wieder vom Feld löschen
                         }
                     }
                 }  
-                returnHint.x = 9;
+                returnHint.x = 9; // kein Gewinn möglich
                 return returnHint;
             }
             public void ResetBoard()
